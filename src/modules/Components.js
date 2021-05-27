684c48028aacc6f8dda868a89084a2fac3d35658
Newohtwo <stasvish1317@gmail.com>
2021-05-27 16:30:53 +0300

    Initial dist subtree commit

--- src/modules/Components.js
+++ src/modules/Components.js
@@ -1,6 +1,6 @@
 //create the logic for the components on the web, button , input
 import DataController from './DataController';
-import api from './WeatherApi';
+
 const copmonents = (() => {
   const searchBtn = initSBtn();
   const inputField = initInputField();
@@ -9,12 +9,12 @@ const copmonents = (() => {
     let element = document.querySelector('#search-btn');
     element.onclick = function () {
       //start the api search for the coordinats and than transform them into the needed data
-      console.log('input field value');
-      console.log(inputField.value);
+
       if (inputField.value === '') {
         return;
       }
       DataController.fetchDataUsingName(inputField.value);
+
       inputField.value = '';
     };
     return element;
